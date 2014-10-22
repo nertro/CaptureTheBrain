914c473a52e9b3f603cfd67a71c119ecfc4832a2
nertro <schneenerdchen@googlemail.com>
2014-10-22 14:32:18 +0200

    Deleted: Old 4.4 Project

--- Source/CapTheBrainProto1/Private/CapTheBrainProto1.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
-
-#include "CapTheBrainProto1.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CapTheBrainProto1, "CapTheBrainProto1" );
- 
