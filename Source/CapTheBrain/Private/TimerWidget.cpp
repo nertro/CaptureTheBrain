4b300058c64a452ef0841116a26b8c5ae2c06283
nertro <schneenerdchen@googlemail.com>
2015-01-12 18:35:14 +0100

    New: Widget für Timer. Timer wird in der Mitte und oben gezeichnet(Position soll von Spieleranzahl abhängen)

---
+++ Source/CapTheBrain/Private/TimerWidget.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CapTheBrain.h"
+#include "TimerWidget.h"
+
+
+UTimerWidget::UTimerWidget(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/CapTheBrain/Public/TimerWidget.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "TimerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPTHEBRAIN_API UTimerWidget : public UUserWidget
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
