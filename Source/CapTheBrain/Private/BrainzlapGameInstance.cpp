c68d45cc383c9cd4438d507415e19c2d5deaee44
nertro <schneenerdchen@googlemail.com>
2015-01-13 23:17:22 +0100

    Fixed: Positionierung für UIBilder an verschiedene Spieleranzahlen angepasst

--- Source/CapTheBrain/Private/BrainzlapGameInstance.cpp
+++ Source/CapTheBrain/Private/BrainzlapGameInstance.cpp
@@ -7,7 +7,7 @@
 UBrainzlapGameInstance::UBrainzlapGameInstance(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
-	playerCount = 3;
+	playerCount = 2;
 }
 
 
--- Source/CapTheBrain/Private/CharacterHUD.cpp
+++ Source/CapTheBrain/Private/CharacterHUD.cpp
@@ -4,6 +4,8 @@
 #include "CharacterHUD.h"
 #include "CapTheBrainCharacter.h"
 #include <vector>
+#include "BrainzlapGameInstance.h"
+#include "EngineUtils.h"
 
 
 
@@ -20,6 +22,15 @@ void ACharacterHUD::BeginPlay()
 	ItemImgs.push_back(ShieldImg);
 	ItemImgs.push_back(SwapImg);
 	ItemImgs.push_back(ZappImg);
+
+	UBrainzlapGameInstance* instance = Cast<UBrainzlapGameInstance>(GetGameInstance());
+	if (instance->playerCount == 1)
+	{
+		UIImgScale = 0.2;
+		UIPaddingBottomRight = 120;
+		UITextPaddingBottom = 120;
+		UITextPaddingBottomRight = 90;
+	}
 }
 
 void ACharacterHUD::DrawBasicUIElements(float screenWidth, float screenHeight)
@@ -27,8 +38,8 @@ void ACharacterHUD::DrawBasicUIElements(float screenWidth, float screenHeight)
 	if (SplatterImg && BrainImg && GUIFont)
 	{
 		float offset = screenWidth / screenHeight;
-		Super::DrawTexture(SplatterImg, UIPaddingLeft, screenHeight - UIPaddingBottomRight, screenWidth, screenHeight, 0, 0, offset, 1, FLinearColor::White, BLEND_Translucent, UIImgScale, false, 0, FVector2D::ZeroVector);
-		Super::DrawTexture(BrainImg, screenWidth - UIPaddingBottomRight, screenHeight - UIPaddingBottomRight, screenWidth, screenHeight, 0, 0, offset, 1, FLinearColor::White, BLEND_Translucent, UIImgScale, false, 0, FVector2D::ZeroVector);
+		Super::DrawTexture(SplatterImg, UIPaddingLeft, screenHeight - (SplatterImg->GetSizeY() * UIImgScale) - UIPaddingBottomRight, screenWidth, screenHeight, 0, 0, offset, 1, FLinearColor::White, BLEND_Translucent, UIImgScale, false, 0, FVector2D::ZeroVector);
+		Super::DrawTexture(BrainImg, screenWidth - (SplatterImg->GetSizeX() * UIImgScale) - UIPaddingBottomRight, screenHeight - (SplatterImg->GetSizeY() * UIImgScale) - UIPaddingBottomRight, screenWidth, screenHeight, 0, 0, offset, 1, FLinearColor::White, BLEND_Translucent, UIImgScale, false, 0, FVector2D::ZeroVector);
 		int score = Cast<ACapTheBrainCharacter>(GetOwningPawn())->GetScore();
 		Super::DrawText(FString::FromInt(score), FLinearColor::Black, screenWidth - UITextPaddingBottomRight, screenHeight - UITextPaddingBottom, GUIFont, UIFontScale, false);
 	}
@@ -41,7 +52,7 @@ void ACharacterHUD::DrawCounterElements(float screenWidth, float screenHeight)
 	{
 		if (ItemImgs[currentItem])
 		{
-			Super::DrawTexture(ItemImgs[currentItem], UIPaddingLeft, screenHeight - UIPaddingBottomRight, screenWidth, screenHeight, 0, 0, offset, 1, FLinearColor::White, BLEND_Translucent, UIImgScale, false, 0, FVector2D::ZeroVector);
+			Super::DrawTexture(ItemImgs[currentItem], UIPaddingLeft, screenHeight - (SplatterImg->GetSizeY() * UIImgScale) - UIPaddingBottomRight, screenWidth, screenHeight, 0, 0, offset, 1, FLinearColor::White, BLEND_Translucent, UIImgScale, false, 0, FVector2D::ZeroVector);
 		}
 	}
 }
