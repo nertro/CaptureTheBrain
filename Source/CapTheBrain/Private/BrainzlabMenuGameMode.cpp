14e77fe5d7357e573429d76407bd6632a869b656
nertro <schneenerdchen@googlemail.com>
2015-01-17 15:05:40 +0100

    New: MenuLevel, Menu GameMode

---
+++ Source/CapTheBrain/Private/BrainzlabMenuGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CapTheBrain.h"
+#include "BrainzlabMenuGameMode.h"
+
+
+ABrainzlabMenuGameMode::ABrainzlabMenuGameMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/CapTheBrain/Public/BrainzlabMenuGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BrainzlabMenuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPTHEBRAIN_API ABrainzlabMenuGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
